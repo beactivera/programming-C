ac9cff6e12831d9f859aa586c6acbb513fe43aa3
beactivera <weronika.wdowiak56@gmail.com>
2020-11-01 21:03:10 +0100

    zadanie 5,6 i 8

---
+++ zadania-3/zad-5.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+ {
+    //  int i;
+    //  printf("Podaj dodatnia liczbe calkowiata\n");
+    //  scanf("%d",&i);
+    //  while (i<=0)
+    //  {
+    //      printf("Podaj ponownie dodatnia liczbe calkowita\n");
+    //      scanf("%d",&i);
+    //  }
+
+
+    // rozwiazanie za pomoca petli for
+
+    // int i;
+    // printf("Podaj dodatnia liczbe calkowiata\n");
+    // scanf("%d",&i);
+
+    // for (int j=0; i<=j;)
+    // {
+    //     printf("Podaj ponownie dodatnia liczbe calkowita\n");
+    //     scanf("%d",&i);
+    // }
+
+    // rozwiazanie za pomoca petli do for
+
+    int i;
+
+    do
+    {
+        printf("Podaj dodatnia liczbe calkowita\n");
+        scanf("%d",&i);
+    }
+    while(i>=0);
+
+    printf("To nie jest liczba calkowita\n Uruchom ponownie program\n");
+
+    return 0;
+
+ }
