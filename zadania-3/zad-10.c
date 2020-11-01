9458e251336f32a01e69ced58b2ece3849094537
beactivera <weronika.wdowiak56@gmail.com>
2020-11-01 22:56:34 +0100

    zadanie 10

---
+++ zadania-3/zad-10.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+ {
+    int i = 0;
+    float liczby[i],a;
+    double suma_liczb = 0,srednia;
+    float wart_min, wart_max; 
+
+    do{
+        printf("Podaj liczbe rzeczywista: ");
+        scanf("%f",&a);
+        liczby[i] = a;
+
+        if(liczby[i]<wart_min)
+        {
+            wart_min = liczby[i];
+        }
+
+        if(liczby[i]>wart_max)
+        {
+            wart_max = liczby[i];  
+        }
+
+        suma_liczb += liczby[i];
+    
+        i++;
+
+    }while(a!=0);
+
+    srednia = suma_liczb/i;
+    
+    printf("Dla podanych liczb suma wynosi: %.2f, srednia arytm.: %.2f, min: %.2f, max: %.2f\n", suma_liczb,srednia,wart_min,wart_max);
+
+    return 0;
+ }
