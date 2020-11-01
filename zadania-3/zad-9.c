9bdf77163fbc97d01463899c521960e86592cd1c
beactivera <weronika.wdowiak56@gmail.com>
2020-11-01 21:41:40 +0100

    zadanie 9

---
+++ zadania-3/zad-9.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+ {
+    int n;
+
+    printf("Podaj liczbe naturalna: ");
+    scanf("%d",&n);
+
+    while(n>0)
+    {
+        printf("Podaj kolejna liczbe naturalna: ");
+        scanf("%d",&n);
+
+    }
+
+    return 0;
+ }
