b6bf337eb9b7ea83dace22225201181cb8fc10cc
beactivera <weronika.wdowiak56@gmail.com>
2020-11-17 15:34:58 +0100

    lab6 - inicjalizacja

---
+++ zadania-6/szablon-B.c
@@ -0,0 +1,22 @@
+#include<time.h>
+#include<stdlib.h>
+#include<stdio.h>
+
+int main()
+{
+	/***** LOSOWANIE TABLICY ******/
+	srand( time( NULL ) );      // inicjalizacja generatora liczb pseudolosowych
+	int tab[20];                // deklaracja tablicy
+	tab[0] = rand() % 5;        // 'wylosuj' liczbe z zakresu 0...4
+	for (int i=1; i<20; i++)    // dla kazdego elementu
+		 tab[i] = tab[i-1] + rand() % 3;   // 'wylosuj' liczbe z zakresu 0...2 i dodaj do poprzedniego elementu
+	
+	/***** Tutaj umieść rozwiązanie zadania ******/    
+	
+	
+	/***** WYŚWIETLENIE ZAWARTOŚCI TABLICY ******/    
+	printf("tab = [");
+	for (int i=0; i<19; i++)
+		 printf("%d, ", tab[i]);
+	printf("%d]\n", tab[19]);
+}
