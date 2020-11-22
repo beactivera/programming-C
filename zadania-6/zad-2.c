4cf687e7c8e39c407e412f96f17657beb310d7f8
beactivera <weronika.wdowiak56@gmail.com>
2020-11-22 23:27:03 +0100

    2 - wystepowanie w tablicy

--- zadania-6/zad-2.c
+++ zadania-6/zad-2.c
@@ -1,36 +1,42 @@
 #include<time.h>
 #include<stdlib.h>
 #include<stdio.h>
+#include<stdbool.h>
 
 int main()
 {
-    int n, licznik = 0;
-
 	/***** LOSOWANIE TABLICY ******/
 	srand( time( NULL ) );      // inicjalizacja generatora liczb pseudolosowych
 	int tab[20];                // deklaracja tablicy
-	tab[0] = rand() % 5;        // 'wylosuj' liczbe z zakresu 0...4
-	for (int i=1; i<20; i++)    // dla kazdego elementu
-		 tab[i] = tab[i-1] + rand() % 3;   // 'wylosuj' liczbe z zakresu 0...2 i dodaj do poprzedniego elementu
+	for (int i=0; i<20; i++)    // dla kazdego elementu
+		 tab[i] = rand() % 20;   // 'wylosuj' liczbe z zakresu 0..19
+	
 	
 	/***** Tutaj umieść rozwiązanie zadania ******/    
-	printf("Podaj szukaną liczbę: ");
-    scanf("%d", &n);
 
-    for(int i=0; i<20; i++){
-        if(n == tab[i])
-            licznik++;
-    }
-    
-    if(licznik>=1)
-        printf("Szukana liczba %d znajduje sie w tablicy.\n",n);
-    else
-        printf("Szukana liczba %d NIE znajduje sie w tablicy.\n",n);
+	printf("Podaj szukany element: ");
+	int x;
+	scanf("%d", &x);
 
-	
+	bool znaleziony = false;
+	for (int i = 0; i<20; i++) {
+		if (tab[i] == x) {
+			znaleziony = true;
+			break;
+		}
+	}
+	if (znaleziony) {
+		printf("Element %d znajduje się w tablicy.\n", x);
+	}
+	else {
+		printf("Element %d NIE znajduje się w tablicy.\n", x);
+	}
+
 	/***** WYŚWIETLENIE ZAWARTOŚCI TABLICY ******/    
-	printf("tab = [");
-	for (int i=0; i<19; i++)
-		 printf("%d, ", tab[i]);
-	printf("%d]\n", tab[19]);
+    printf("tab = [");
+    for (int i=0; i<19; i++)
+         printf("%d, ", tab[i]);
+    printf("%d]\n", tab[19]);
+
+	return 0;
 }
