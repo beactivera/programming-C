b3394314601a451299ad724d04a5f77aca72c10c
beactivera <weronika.wdowiak56@gmail.com>
2020-11-22 13:56:37 +0100

    8 - sumy prefiksowe

--- zadania-5/zad-8.c
+++ zadania-5/zad-8.c
@@ -4,7 +4,7 @@
 
 int main()
 {
-    int suma_prefiksowa = 0;
+    int suma_prefiksowa = 0, nowa_tablica[20];
 	/***** LOSOWANIE TABLICY ******/
 	srand( time( NULL ) );      // inicjalizacja generatora liczb pseudolosowych
 	int tab[20];                // deklaracja tablicy
@@ -13,9 +13,17 @@ int main()
 	
 	
 	/***** Tutaj umieść rozwiązanie zadania ******/
-    for(int i=0; i<20; i++){    
+    for(int i=0; i<20; i++){   
+		nowa_tablica[i] = suma_prefiksowa; 
 	    printf("Niech i = %d, tab[%d] = %d, to suma_prefiksowa[%d] = %d \n", i,i,tab[i],i, suma_prefiksowa);
         suma_prefiksowa += tab[i];
     }
+
+	printf("Twoja tablica sum prefiksowych: [");
+	for (int i=0; i<20; i++)
+		 printf("%d, ", nowa_tablica[i]);
+	printf("%d]\n", nowa_tablica[19]);
+
+	return 0;
 }
 
