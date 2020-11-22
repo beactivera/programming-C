f8642de69919a90b5f5ad626a755d62eed678e33
beactivera <weronika.wdowiak56@gmail.com>
2020-11-22 23:40:39 +0100

    10 - boubble sort - drugi max

--- zadania-5/zad-10.c
+++ zadania-5/zad-10.c
@@ -1,48 +1,79 @@
 #include<time.h>
 #include<stdlib.h>
 #include<stdio.h>
+#include<stdbool.h>
 
-int find_maximum(int[], int);
-int find_minimum(int[], int);
-int find_mean(int[], int);
+void skopiuj_tablice(int[], int[], int);
+void posortuj_tablice(int[], int);
+bool swap(int*, int*);
 
 int main()
 {
-    int n=20,tab[n],location_max, location_max2, maximum, maximum2;
-	/***** LOSOWANIE TABLICY ******/
-	srand( time( NULL ) );      // inicjalizacja generatora liczb pseudolosowych
-	for (int i=0; i<20; i++)    // dla kazdego elementu
-		 tab[i] = rand() % 20;   // 'wylosuj' liczbe z zakresu 0..19
-	
-    /***** WYŚWIETLENIE ZAWARTOŚCI TABLICY ******/    
-	printf("wylosowana tablica = [");
-	for (int i=0; i<19; i++)
-		 printf("%d, ", tab[i]);
-	printf("%d]\n", tab[19]);
-	
-	/***** Tutaj umieść rozwiązanie zadania ******/ 
-	location_max = find_maximum(tab,n);
-    maximum  = tab[location_max];
-
-
-    for(int j = location_max; j < n-1; j++){
-        tab[j]  = tab[j+1];
-    } 
-
-    location_max2 = find_maximum(tab,n-1);
-    maximum2  = tab[location_max2];
-
-    printf("Maksymalny element na pozycji %d ma wartosc %d.\n", location_max + 1, maximum);
-    printf("Drugi maksymalny element na pozycji %d ma wartosc %d.\n", location_max2 + 1, maximum2);
+    int dlugosc_tablicy = 20;
+    /***** LOSOWANIE TABLICY ******/
+    srand( time( NULL ) );      // inicjalizacja generatora liczb pseudolosowych
+    int tab[dlugosc_tablicy];                // deklaracja tablicy
+    for (int i=0; i<dlugosc_tablicy; i++)    // dla kazdego elementu
+      tab[i] = rand() % 20;   // 'wylosuj' liczbe z zakresu 0..19
     
+    
+    /***** Tutaj umieść rozwiązanie zadania ******/    
+    int tab_posortowana[dlugosc_tablicy];
+      
+      skopiuj_tablice(tab, tab_posortowana, dlugosc_tablicy);
+      posortuj_tablice(tab_posortowana, dlugosc_tablicy);
+
+    /***** WYŚWIETLENIE ZAWARTOŚCI TABLICY ******/    
+    printf("tab = [");
+    for (int i=0; i<dlugosc_tablicy-1; i++)
+      printf("%d, ", tab[i]);
+    printf("%d]\n", tab[dlugosc_tablicy-1]);
+      
+    printf("tab_posortowana = [");
+    for (int i=0; i<dlugosc_tablicy-1; i++)
+      printf("%d, ", tab_posortowana[i]);
+    printf("%d]\n", tab_posortowana[dlugosc_tablicy-1]);
+
+    int maks = tab_posortowana[dlugosc_tablicy-1];
+
+    int drugie_maks;
+    for (int i = dlugosc_tablicy-2; i>=0; i--) {
+      if (tab_posortowana[i] != maks) {
+        drugie_maks = tab_posortowana[i];
+        break;
+      }
+    }
+    printf("Druga maksymalna wartosc to: %d.\n", drugie_maks);
+    
+    return 0;
 }
 
-int find_maximum(int a[], int n) {
-  int c, index = 0;
 
-  for (c = 1; c < n; c++)
-    if (a[c] > a[index])
-      index = c;
+void skopiuj_tablice(int tab1[], int tab2[], int n) {
+    for (int i=0; i<n; i++) {
+        tab2[i] = tab1[i];
+    }     
+}
 
-  return index;
+void posortuj_tablice(int tab[], int n) {
+    int swaps;
+    do {
+        swaps = 0;
+        for (int i = 0; i<n-1; i++) {
+            if (swap(&tab[i], &tab[i+1])) {
+                swaps += 1;
+            }
+        }
+
+    } while (swaps != 0);
+}
+
+bool swap(int* a, int* b) {
+    if (*a > *b) {
+        int tmp = *a;
+        *a = *b;
+        *b = tmp;
+        return true;
+    }
+    return false;
 }
