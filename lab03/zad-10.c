25dbc7d8418e4f4f147eea824de25a2a24fb3295
Weronika Wdowiak <weronika.wdowiak56@gmail.com>
2020-12-07 11:30:11 +0100

    wczytywanie liczb do podania 0, wyliczenie min, max,avg i max2

--- lab03/zad-10.c
+++ lab03/zad-10.c
@@ -6,26 +6,32 @@ int main()
     int i = 0;
     float liczby[i],a;
     double suma_liczb = 0,srednia;
-    float wart_min, wart_max; 
+    float wart_min=1000, wart_max=0; 
 
     do{
         printf("Podaj liczbe rzeczywista: ");
         scanf("%f",&a);
-        liczby[i] = a;
 
-        if(liczby[i]<wart_min)
-        {
-            wart_min = liczby[i];
+        if(a == 0){
+            break;
         }
+        else{
+            liczby[i] = a;
 
-        if(liczby[i]>wart_max)
-        {
-            wart_max = liczby[i];  
+            if(liczby[i]<wart_min)
+            {
+                wart_min = liczby[i];
+            }
+
+            if(liczby[i]>wart_max)
+            {
+                wart_max = liczby[i];  
+            }
+
+            suma_liczb += liczby[i];
+        
+            i++;
         }
-
-        suma_liczb += liczby[i];
-    
-        i++;
 
     }while(a!=0);
 
