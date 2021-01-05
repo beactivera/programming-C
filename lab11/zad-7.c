3b6b9ce4c261f23ffe58e952db2b8053f94024dc
beactivera <weronika.wdowiak56@gmail.com>
2021-01-05 13:54:12 +0100

    obliczanie ulamkow przy pomocy 'struct'

--- lab11/zad-7.c
+++ lab11/zad-7.c
@@ -1,32 +1,37 @@
-#include <stdio.h>
+#include <stdio.h> 
 struct Ulamek
 {
     int licznik;
     int mianownik;
 };
 
-void skracanie(struct Ulamek ulamek);
-void suma(struct Ulamek ulamek1, struct Ulamek ulamek2);
-void roznica(struct Ulamek ulamek1, struct Ulamek ulamek2);
-void iloczyn(struct Ulamek ulamek1, struct Ulamek ulamek2);
-void iloraz(struct Ulamek ulamek1, struct Ulamek ulamek2);
+struct Ulamek skracanie(struct Ulamek);
+struct Ulamek suma(struct Ulamek, struct Ulamek);
+struct Ulamek roznica(struct Ulamek, struct Ulamek);
+struct Ulamek mnozenie(struct Ulamek, struct Ulamek);
+struct Ulamek dzielenie(struct Ulamek, struct Ulamek);
 
-int main(){
+int main() {
 
-    struct Ulamek u1 = {1, 2},
+    struct Ulamek u1 = {1,2},
                   u2 = {1,3},
                   u3 = {7,2},
                   u4 = {2,3},
                   u5 = {10,3};
-    struct Ulamek u6;
-    
-    u6 = suma(u1+u2);
-    printf("%d/%d",u6.licznik, u6.mianownik);
-
-
+    struct Ulamek u6,u7,u8,u9,u10;
+    u6 = suma(u1, u2);
+    printf("%d/%d\n",u6.licznik, u6.mianownik);
+    u7 = dzielenie(u3, u4);
+    printf("%d/%d\n",u7.licznik, u7.mianownik);
+    u8 = mnozenie(u6, u7);
+    printf("%d/%d\n",u8.licznik, u8.mianownik);
+    u9 = roznica(u8, u5);
+    printf("%d/%d\n",u9.licznik, u9.mianownik);
+    u10 = skracanie(u9);
+    printf("%d/%d",u10.licznik, u10.mianownik);
 }
 
-void skracanie(struct Ulamek ulamek){
+struct Ulamek skracanie(struct Ulamek ulamek){
     int a=2;
     do
     {
@@ -34,46 +39,45 @@ void skracanie(struct Ulamek ulamek){
         {
             ulamek.licznik /= a;
             ulamek.mianownik /= a;
-            a--;
+            a=2;
         }
-        a++;
+        else{
+            a++;
+        }
+        
     } while(a<ulamek.mianownik && a<ulamek.licznik);
 
     return ulamek;
-    
 }
 
-void suma(struct Ulamek u1, struct Ulamek u2){
+struct Ulamek suma(struct Ulamek u1, struct Ulamek u2){
     struct Ulamek u;
     u.licznik = u1.licznik*u2.mianownik + u2.licznik*u1.mianownik;
     u.mianownik = u1.mianownik* u2.mianownik;
 
-    skracanie(u);
+    return u;
 }
 
-void roznica(struct Ulamek u1, struct Ulamek u2){
+struct Ulamek roznica(struct Ulamek u1, struct Ulamek u2){
     struct Ulamek u;
     u.licznik = u1.licznik*u2.mianownik - u2.licznik*u1.mianownik;
     u.mianownik = u1.mianownik* u2.mianownik;
 
-    skracanie(u);
+    return u;
 }
 
-void mnozenie(struct Ulamek u1, struct Ulamek u2){
+struct Ulamek mnozenie(struct Ulamek u1, struct Ulamek u2){
     struct Ulamek u;
     u.licznik = u1.licznik*u2.licznik;
     u.mianownik = u1.mianownik*u2.mianownik;
 
-    skracanie(u);
+    return u;
 }
 
-void dzielenie(struct Ulamek u1, struct Ulamek u2){
+struct Ulamek dzielenie(struct Ulamek u1, struct Ulamek u2){
     struct Ulamek u;
     u.licznik = u1.licznik*u2.mianownik;
     u.mianownik = u1.mianownik*u2.licznik;
 
-    skracanie(u);
+    return skracanie(u);
 }
-
-
-
