cdfbd2ac120e2e92a6460df2de8d14e7441a535a
beactivera <weronika.wdowiak56@gmail.com>
2021-01-10 14:44:25 +0100

    struktura ksiazek - dodawanie,odejmowanie i szukanie

--- lab11/zad-6.c
+++ lab11/zad-6.c
@@ -1,37 +1,37 @@
 #include <stdio.h>
 #include <string.h>
-struct Book
-{
+
+typedef struct{
     char title[20];
     char author[20];
     int  year;
     int  pages; 
-};
+}ksiazka;
 
-void display(struct Book book);
-void dodajKsiazke(struct Book baza_ksiazek[], int* aktualna_ilosc_ksiazek);
-struct Book* usunKsiazke(struct Book baza_ksiazek[], int* aktualna_ilosc_ksiazek);
-void szukajPoRoku(struct Book baza_ksiazek[]);
-void szukajPoAutorze(struct Book baza_ksiazek[]);
+void display(ksiazka ksiazki[]);
+void dodaj(ksiazka ksiazki[], int* pozycja);
+void usun(ksiazka ksiazki[], int* pozycja);
+void szukajPoAutorze(ksiazka ksiazki[], int pozycja);
+void szukajPoRoku(ksiazka ksiazki[], int pozycja);
 
-const int maks_ilosc = 20;
 
 int main(){
-    int aktualna_ilosc_ksiazek = 0;
-    struct Book baza_ksiazek[maks_ilosc];
+    int pozycja = 0;
+    ksiazka ksiazki[20] = {0};
+
 
     while(1) {
-        printf("INFORMACJE O TWOICH KSIAZKACH\nWybierz jedna z ponizszych opcji:\n  1 - dodaj ksiazke\n  2 - odejmij ksiazke\n  3 - wyszukaj po roku\n  4 - wyszukaj po roku\n");
+        printf("INFORMACJE O TWOICH KSIAZKACH\nWybierz jedna z ponizszych opcji:\n  1 - dodaj ksiazke\n  2 - odejmij ksiazke\n  3 - wyszukaj po roku\n  4 - wyszukaj po autorze\n");
 
         int cyfra;
         scanf("%d",&cyfra);
 
-        if (cyfra >= 1 && cyfra < 4){
+        if (cyfra >= 1 && cyfra <= 4){
             switch(cyfra) {
-            case 1 : dodajKsiazke(baza_ksiazek, &aktualna_ilosc_ksiazek); break;
-            case 2 : baza_ksiazek = usunKsiazke(baza_ksiazek, &aktualna_ilosc_ksiazek); break;
-            case 3 : szukajPoRoku(baza_ksiazek); break;
-            case 4 : szukajPoAutorze(baza_ksiazek); break;
+            case 1 : dodaj(ksiazki, &pozycja); break;
+            case 2 : usun(ksiazki, &pozycja); break;
+            case 3 : szukajPoRoku(ksiazki,pozycja); break;
+            case 4 : szukajPoAutorze(ksiazki,pozycja); break;
             }
         }
         else {
@@ -39,81 +39,70 @@ int main(){
             scanf("%d",&cyfra);
         }
     }
-    // struct Book arrBooks[5]= {
-    //     {"Romeo i Julia", "William Shakespeare", 1595, 120},
-    //     {"Hamlet", "William Shakespeare", 1600, 180},
-    //     {"Carrie", "Stephen King", 1974, 405},
-    //     {"To", "Stephen King", 1986, 399},
-    //     {"Nie mów nikomu", "Harlan Coben",2001, 278}
-    // };
-
-    // int n = sizeof arrBooks/ sizeof arrBooks[0];
-    // for(int i=0; i<n; i++){
-    //     display(arrBooks[i]);
-    // }
     return 0;
 }
 
-void display(struct Book book){
-        printf("title: %s\n", book.title);
-        printf("author: %s\n", book.author);
-        printf("year: %d\n", book.year);
-        printf("pages: %d\n", book.pages);
+void display(ksiazka ksiazki[]){
+        printf("title: %s\n", ksiazki[0].title);
+        printf("author: %s\n", ksiazki[0].author);
+        printf("year: %d\n", ksiazki[0].year);
+        printf("pages: %d\n", ksiazki[0].pages);
         printf("\n");
 }
 
-void dodajKsiazke(struct Book baza_ksiazek[], int* aktualna_ilosc_ksiazek){
+
+void dodaj(ksiazka ksiazki[], int *pozycja) {
+    char title[20], author[20];
+    int year, pages;
     printf("Podaj tytul: ");
-    scanf("&s", baza_ksiazek[*aktualna_ilosc_ksiazek].title);
+    scanf("%20s", title);
     printf("Podaj autora: ");
-    scanf("&s", baza_ksiazek[*aktualna_ilosc_ksiazek].author);
+    scanf("%20s", author);
     printf("Podaj rok wydania: ");
-    scanf("&d", &baza_ksiazek[*aktualna_ilosc_ksiazek].year);
-    printf("Podaj ilosc stron: ");
-    scanf("&d", &baza_ksiazek[*aktualna_ilosc_ksiazek].pages);
-    *aktualna_ilosc_ksiazek++;
+    scanf("%d", &year);
+    printf("Podaj liczbe stron: ");
+    scanf("%d",&pages);
+    strcpy(ksiazki[*pozycja].title, title);
+    strcpy(ksiazki[*pozycja].author, author);
+    ksiazki[*pozycja].year = year;
+    ksiazki[*pozycja].pages = pages;
+    (*pozycja)++;
 }
 
-struct Book* usunKsiazke(struct Book baza_ksiazek[], int* aktualna_ilosc_ksiazek){
-    struct Book nowa_baza[maks_ilosc];
-    int ilosc_w_nowej_bazie = 0;
+void usun(ksiazka ksiazki[], int *pozycja) {
+    int temp = *pozycja;
     char title[20];
     printf("Podaj tytul: ");
-    scanf("%s", title);
-    for(int i=0; i<*aktualna_ilosc_ksiazek; i++){
-        if(!strcmp(baza_ksiazek[i].title, title)){
-            nowa_baza[ilosc_w_nowej_bazie] = baza_ksiazek[i];
-            ilosc_w_nowej_bazie++;
+    scanf("%20s", title);
+    while (temp--){
+        if (strcmp(ksiazki[temp].title, title) == 0){
+            for(int i=temp; i<*pozycja; i++){
+                memcpy(&ksiazki[i], &ksiazki[i+1], sizeof(ksiazki[i+1]));
+            };
+            (*pozycja)--;
         }
     }
-    baza_ksiazek = nowa_baza;
-    *aktualna_ilosc_ksiazek = ilosc_w_nowej_bazie;
-    for(int i=0; i<*aktualna_ilosc_ksiazek; i++){
-        display(baza_ksiazek[i]);
-    }
-    return baza_ksiazek;
 }
+    
 
-void szukajPoRoku(struct Book arrBooks[]){
+void szukajPoRoku(ksiazka ksiazki[], int pozycja){
+    printf("Rok: ");
     int year;
-    printf("Podaj rok: ");
-    scanf("%d", year );
-    int n = sizeof arrBooks/ sizeof arrBooks[0];
-    for(int i=0; i<n;i++){
-        if(arrBooks[i].year == year){
-            display(arrBooks[i]);
+    scanf("%d", &year);
+    while (pozycja--){
+        if (ksiazki[pozycja].year == year){
+            display(ksiazki + pozycja);
         }
     }
 }
 
-void szukajPoAutorze(struct Book arrBooks[]){
-    char author;
-    printf("Podaj rok: ");
-    scanf("%s", author );
-    int n = sizeof arrBooks/ sizeof arrBooks[0];
-    for(int i=0; i<n;i++){
-        if(arrBooks[i].author == author){
-            display(arrBooks[i]);
+void szukajPoAutorze(ksiazka ksiazki[], int pozycja) {
+    printf("Autor: ");
+    char author[20];
+    scanf("%20s", author);
+    while (pozycja--){
+        if (strcmp(ksiazki[pozycja].author, author) == 0){
+            display(ksiazki + pozycja);
         }
     }
 }
